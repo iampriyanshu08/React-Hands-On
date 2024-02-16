9ea51b28d6ccfa688be35a2e9b5f19d17cc82627
Priyanshu Utsahi <utsahiji.priyanshu2000@gmail.com>
2024-02-17 00:32:41 +0530

    tailwind practise

---
+++ React-Lec/TalwindCLI/tailwind.config.js
@@ -0,0 +1,7 @@
+/** @type {import('tailwindcss').Config} */
+export const content = ["./src/**/*.html"];
+export const theme = {
+  extend: {},
+};
+export const plugins = [];
+
---
+++ React-Lec/TalwindWithReact/my-project/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <h1 className='py-16  text-[5rem] font-bold text-[orange] sm:text-[blue]'>Hello Vite React App</h1>
+    </div>
+  )
+}
+
+export default App
